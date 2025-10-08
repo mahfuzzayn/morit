614974b194627da5d675250922e514ec6ff9e79d
Mahfuz Zayn <mahfuzzayn8@gmail.com>
2025-10-08 14:15:14 +0600

    Updated /agency route, Added /unauthorized route and Sidebar Integration based on user role

---
+++ src/components/sidebar/menu-options.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import {
+    AgencySidebarOption,
+    SubAccount,
+    SubAccountSidebarOption,
+} from "@prisma/client";
+import React, { useEffect, useMemo, useState } from "react";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Button } from "../ui/button";
+import { ChevronsUpDown, Compass, Menu } from "lucide-react";
+import clsx from "clsx";
+import { AspectRatio } from "../ui/aspect-ratio";
+import Image from "next/image";
+import { Popover, PopoverTrigger } from "../ui/popover";
+
+type Props = {
+    defaultOpen?: boolean;
+    subAccounts: SubAccount[];
+    sidebarOpt: AgencySidebarOption[] | SubAccountSidebarOption[];
+    sidebarLogo: string;
+    details: any;
+    user: any;
+    id: string;
+};
+
+const MenuOptions = ({
+    details,
+    id,
+    sidebarLogo,
+    sidebarOpt,
+    subAccounts,
+    user,
+    defaultOpen,
+}: Props) => {
+    const [isMounted, setIsMounted] = useState(false);
+    const openState = useMemo(
+        () => (defaultOpen ? { open: true } : {}),
+        [defaultOpen]
+    );
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) return;
+
+    return (
+        <Sheet modal={false} open={true}>
+            <SheetTrigger
+                asChild
+                className="absolute left-4 top-4 z-[100] md:!hidden"
+            >
+                <Button variant="outline" size={"icon"}>
+                    <Menu />
+                </Button>
+            </SheetTrigger>
+            <SheetContent
+                showX={!defaultOpen}
+                side={"left"}
+                className={clsx(
+                    "bg-background/80 backdrop-blur-xl fixed top-0 border-r-[1px] p-6",
+                    {
+                        "hidden md:inline-block z-0 w-[300px]": defaultOpen,
+                        "inline-block md:hidden z-[100] w-full": !defaultOpen,
+                    }
+                )}
+            >
+                <div>
+                    <AspectRatio ratio={16 / 5}>
+                        <Image
+                            src={sidebarLogo}
+                            alt="Sidebar Logo"
+                            fill
+                            className="rounded-md object-contain"
+                        />
+                    </AspectRatio>
+                    <Popover>
+                        <PopoverTrigger asChild>
+                            <Button
+                                className="w-full my-4 flex items-center justify-between"
+                                variant={"ghost"}
+                            >
+                                <div className="flex items-center text-left gap-2">
+                                    <Compass />
+                                    <div className="flex flex-col">
+                                        {details.name}
+                                        <span className="text-muted-foreground">
+                                            {details.address}
+                                        </span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <ChevronsUpDown
+                                        size={16}
+                                        className="text-muted-foreground"
+                                    />
+                                </div>
+                            </Button>
+                        </PopoverTrigger>
+                    </Popover>
+                </div>
+            </SheetContent>
+        </Sheet>
+    );
+};
+
+export default MenuOptions;
